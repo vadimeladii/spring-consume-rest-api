e19be6a37c913b873e9f6fdd70fb6e2e9866aedd
vadimeladii <vadimeladii@gmail.com>
2020-01-10 21:32:22 +0200

    Imit project

---
+++ src/main/java/md/springboot/SpringConsumeRestApiApplication.java
@@ -0,0 +1,13 @@
+package md.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringConsumeRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringConsumeRestApiApplication.class, args);
+    }
+
+}
